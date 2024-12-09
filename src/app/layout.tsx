13678a0ee0569938e89ee96982054fb3614a19a4
Martín Stanicio <martuytimo@gmail.com>
2024-12-09 17:08:23 -0300

    update theme colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { cn } from "@/lib/utils";
+
 import "./globals.css";
 
 const bodyFont = Inter({ subsets: ["latin"] });
@@ -17,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={`${bodyFont.className} antialiased`}>{children}</body>
+      <body className={cn("dark antialiased", bodyFont.className)}>
+        {children}
+      </body>
     </html>
   );
 }
