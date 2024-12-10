c8650dceaa173524c06289a717937ae8e660aa37
Martín Stanicio <martuytimo@gmail.com>
2024-12-10 01:28:12 -0300

    add burgers

---
+++ content/burgers.ts
@@ -0,0 +1,46 @@
+import datsun240z from "@/assets/datsun-240z.png";
+import imprezawrxsti from "@/assets/impreza-wrx-sti.png";
+import mazdarx7 from "@/assets/mazda-rx-7.png";
+import silvias15 from "@/assets/silvia-s15.png";
+import skylinegtr from "@/assets/skyline-gt-r.png";
+import supra2jzgte from "@/assets/supra-2jz-gte.png";
+
+const burgers = [
+  {
+    name: "Skyline GT-R",
+    description:
+      "Dos fetas de cheddar por medallón, acompañado de un crocante bacon.",
+    image: skylinegtr,
+  },
+  {
+    name: "Datsun 240Z",
+    description:
+      "Dos fetas de cheddar por medallón, ketchup y mostaza, acompañado de cebolla cortada en cubitos.",
+    image: datsun240z,
+  },
+  {
+    name: "Mazda RX-7",
+    description: "Dos fetas de cheddar por medallón, lechuga, tomate y huevo.",
+    image: mazdarx7,
+  },
+  {
+    name: "Supra 2JZ-GTE",
+    description:
+      "Dos fetas de cheddar por medallón, lechuga, tomate y cebolla cortada en aros.",
+    image: supra2jzgte,
+  },
+  {
+    name: "Impreza WRX STI",
+    description:
+      "Dos fetas de cheddar por medallón, acompañado de un crocante bacon y cebolla a la plancha.",
+    image: imprezawrxsti,
+  },
+  {
+    name: "Silvia S15",
+    description:
+      "Dos fetas de cheddar por medallón, acompañado de cebolla cocinada a la plancha.",
+    image: silvias15,
+  },
+];
+
+export default burgers;
