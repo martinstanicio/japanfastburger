5ce83678b7a3a3442f5b5c38a94e19bf87cdd54e
Martín Stanicio <martuytimo@gmail.com>
2024-12-10 01:30:39 -0300

    setup homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Hero from "@/components/hero";
+import Menu from "@/components/menu";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
-    <main>
+    <main className="space-y-8">
       <Hero />
+      <Separator />
+      <section className="p-4 sm:p-8">
+        <h2 className="text-center text-3xl font-bold md:text-4xl">
+          Hamburguesas
+        </h2>
+        <Menu />
+      </section>
     </main>
   );
 }
