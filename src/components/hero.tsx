36625172ffb91889874f0dd16b13ba2ee0ffc61a
Martín Stanicio <martuytimo@gmail.com>
2024-12-09 23:53:46 -0300

    create hero component

---
+++ src/components/hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <section className="space-y-8 p-8">
+      <h1 className="text-balance text-center text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
+        Japan Fast Burger
+      </h1>
+      <div className="mx-auto aspect-square w-72 max-w-full rounded bg-red-500"></div>
+    </section>
+  );
+}
