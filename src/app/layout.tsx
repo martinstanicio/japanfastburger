5d68cee8a1ef6dfa57f96c716869d21f1dd7e179
Martín Stanicio <martuytimo@gmail.com>
2024-12-13 22:28:25 -0300

    create footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import { cn } from "@/lib/utils";
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={cn("dark antialiased", bodyFont.className)}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
