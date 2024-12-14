7f8a96e040228e31eb744b5a28a64e9461194fde
Martín Stanicio <martuytimo@gmail.com>
2024-12-13 21:18:30 -0300

    update fries menu grid

--- src/components/fries-menu.tsx
+++ src/components/fries-menu.tsx
@@ -4,7 +4,7 @@ import FriesCard from "./fries-card";
 
 export default function FriesMenu() {
   return (
-    <section className="mx-auto grid max-w-6xl gap-8 sm:grid-cols-2 lg:grid-cols-3">
+    <section className="mx-auto grid max-w-6xl gap-8 sm:grid-cols-2 xl:grid-cols-4">
       {fries.map((friesProps, i) => (
         <FriesCard key={i} {...friesProps} />
       ))}
