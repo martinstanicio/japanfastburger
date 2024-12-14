4a780d518bb870598c7da9d45e6f37e004bf9b47
Martín Stanicio <martuytimo@gmail.com>
2024-12-13 22:14:19 -0300

    move social links to standalone file

---
+++ content/links.ts
@@ -0,0 +1,16 @@
+import { faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+
+const links = [
+  {
+    name: "WhatsApp",
+    href: "https://api.whatsapp.com/send?phone=5493484333962",
+    icon: faWhatsapp,
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/japanfastburger/",
+    icon: faInstagram,
+  },
+];
+
+export default links;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,7 @@
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+
+import links from "~/links";
 
 import Brand from "./brand";
 import { Button } from "./ui/button";
@@ -10,19 +11,27 @@ export default function Navbar() {
     <nav className="flex items-center justify-between p-4 sm:p-8">
       <Brand />
 
-      <Button size="icon" asChild className="sm:hidden">
-        <Link href="https://www.instagram.com/japanfastburger/">
-          <FontAwesomeIcon icon={faInstagram} />
-          <span className="sr-only">Instagram</span>
-        </Link>
-      </Button>
+      <div className="flex gap-2 sm:hidden">
+        {links.map(({ name, href, icon }, i) => (
+          <Button size="icon" asChild key={i}>
+            <Link href={href} target="_blank">
+              <FontAwesomeIcon icon={icon} />
+              <span className="sr-only">{name}</span>
+            </Link>
+          </Button>
+        ))}
+      </div>
 
-      <Button asChild className="max-sm:hidden">
-        <Link href="https://www.instagram.com/japanfastburger/">
-          <FontAwesomeIcon icon={faInstagram} />
-          <span>Instagram</span>
-        </Link>
-      </Button>
+      <div className="flex items-center gap-2 max-sm:hidden">
+        {links.map(({ name, href, icon }, i) => (
+          <Button asChild key={i}>
+            <Link href={href} target="_blank">
+              <FontAwesomeIcon icon={icon} />
+              <span>{name}</span>
+            </Link>
+          </Button>
+        ))}
+      </div>
     </nav>
   );
 }
